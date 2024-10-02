b6503c12384cc40232d676257d8b36cc61b7eeb8
Kele Marton <kele.marton.robert@diak.szbi-pg.hu>
2024-10-02 11:28:44 +0200

    Calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def calculate_sum(a, b):
+    return a + b
+
+def calculate_sub(a, b):
+    return a - b
+
+def calculate_multi(a, b):
+    return a * b
+
+def calculate_div(a, b):
+    return a / b
+
+write("Első Szám: ")
+num1 = int(input())
